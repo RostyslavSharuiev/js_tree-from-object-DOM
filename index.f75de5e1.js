ad3c7ccc1c53e0a7e68e635ef02a6438eedd86bd
Rostyslav <rostislavkpi@gmail.com>
2024-08-15 13:41:15 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ul = document.createElement("ul");
+    Object.entries(data).forEach(([key, nestedData])=>{
+        const li = document.createElement("li");
+        li.textContent = key;
+        if (Object.keys(nestedData).length) createTree(li, nestedData);
+        ul.appendChild(li);
+    });
+    element.appendChild(ul);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
